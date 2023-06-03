a121d123348f4bcf7ff3a31787290ea5d35073ad
Kripesh Thapaliya <Kripeshthapaliya@gmail.com>
2023-06-03 02:21:28 -0400

    formating done

--- Assignment/Pack.cs
+++ Assignment/Pack.cs
@@ -1,42 +1,163 @@
-﻿namespace Assignment;
+﻿using System.Diagnostics;
 
-class Pack
+namespace Assignment;
+public class Pack
 {
-    private InventoryItem[] _items; // You can use another data structure here if you prefer.
+    private readonly InventoryItem[] _items; // You can use another data structure here if you prefer.
     // You may need another private member variable if you use an array data structure.
-
-    private int _maxCount;
-    private float _maxVolume;
-    private float _maxweight;
-
+    private readonly int _maxCount;
+    private readonly float _maxVolume;
+    private readonly float _maxWeight;
+    private int _currentCount; // Defaults to 0
+    private float _currentVolume;
+    private float _currentWeight;
+    // Default constructor sets the maxCount to 10 
+    // maxVolume to 20 
+    // maxWeight to 30
+    public Pack() : this(10, 20, 30) { }
+    // This constructor is not complete, but it is a good start.
     public Pack(int maxCount, float maxVolume, float maxWeight)
     {
+        _items = new InventoryItem[maxCount];
         _maxCount = maxCount;
-
+        _maxVolume = maxVolume;
+        _maxWeight = maxWeight;
     }
-
+    // This is called a getter
+    public int GetMaxCount()
+    {
+        return _maxCount;
+    }
+    public float GetVolume()
+    {
+        return _currentVolume;
+    }
+    public float GetWeight()
+    {
+        return _currentWeight;
+    }
+    public int GetCurrentCount()
+    {
+        return _currentCount;
+    }
+    public float GetCurrentVolume()
+    {
+        return _currentVolume;
+    }
+    public float GetCurrentWeight()
+    {
+        return _currentWeight;
+    }
     public bool Add(InventoryItem item)
     {
-        throw new NotImplementedException();
+        float weight = item.GetWeight();
+        float volume = item.GetVolume();
+        if (volume <= _maxVolume)
+        {
+            _currentWeight += weight;
+            _currentVolume += volume;
+            _items[_currentCount++] = item;
+            return true;
+        }
+        return false;
     }
-
-    // Implement this class
     public override string ToString()
     {
-        throw new NotImplementedException();
+        string packContents = $"Pack is currently at {_currentCount}/{_maxCount} items, {_currentWeight}/{_maxWeight} weight, and {_currentVolume}/{_maxVolume} volume.";
+
+        return packContents;
+    }
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
     }
 }
-
-class InventoryItem
+// Come back to this once we learn about abstract classes.
+public abstract class InventoryItem
 {
-    // Implement this class
+    private readonly float _volume;
+    private readonly float _weight;
+    protected InventoryItem(float volume, float weight)
+    {
+        if (volume <= 0f || weight <= 0f)
+        {
+            throw new ArgumentOutOfRangeException($"An item can't have {volume} volume or {weight} weight");
+        }
+        _volume = volume;
+        _weight = weight;
+    }
+    // Returns a string representing the quantities of the item (volume & weight of the item)
+    public abstract string Display();
+    public override bool Equals(object? obj)
+    {
+        return base.Equals(obj);
+    }
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+    // Getters
+    public float GetVolume()
+    {
+        return _volume;
+    }
+    public float GetWeight()
+    {
+        return _weight;
+    }
+    public override string? ToString()
+    {
+        return base.ToString();
+    }
 }
-
 // Implement these classes - each inherits from InventoryItem
 // 1 line of code each - call base class constructor with appropriate arguments
-class Arrow : InventoryItem { }
-class Bow : InventoryItem { }
-class Rope : InventoryItem { }
-class Water : InventoryItem { }
-class Food : InventoryItem { }
-class Sword : InventoryItem { }
+public class Arrow : InventoryItem
+{
+    public Arrow() : base(0.5f, 0.1f) { }
+    public override string Display()
+    {
+        return $"An arrow with weight {GetWeight()} and volume {GetVolume()}";
+    }
+}
+public class Bow : InventoryItem
+{
+    public Bow() : base(1f, 4f) { }
+    public override string Display()
+    {
+        return $"A bow with weight {GetWeight()} and volume {GetVolume()}";
+    }
+}
+public class Rope : InventoryItem
+{
+    public Rope() : base(1f, 1.5f) { }
+    public override string Display()
+    {
+        return $"A rope with weight {GetWeight()} and volume {GetVolume()}";
+    }
+}
+public class Water : InventoryItem
+{
+    public Water() : base(2f, 3f) { }
+    public override string Display()
+    {
+        return $"Water with weight {GetWeight()} and volume {GetVolume()}";
+    }
+}
+public class Food : InventoryItem
+{
+    public Food() : base(1f, 0.5f) { }
+    public override string Display()
+    {
+        return $"Yummy food with weight {GetWeight()} and volume {GetVolume()}";
+    }
+}
+public class Sword : InventoryItem
+{
+    public Sword() : base(5f, 3f) { }
+
+    public override string Display()
+    {
+        return $"A sharp sword with weight {GetWeight()} and volume {GetVolume()}";
+    }
+}
--- AssignmentTest/AssignmentTest.cs
+++ AssignmentTest/AssignmentTest.cs
@@ -1,5 +1,4 @@
 ﻿using Assignment;
-
 namespace AssignmentTest
 {
     [TestClass]
@@ -10,10 +9,68 @@ namespace AssignmentTest
         {
             const int PackMaxItems = 10;
             const float PackMaxVolume = 20;
-            const float PackMaxWeight = 20;
+            const float PackMaxWeight = 30;
             Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
-
-            Assert.AreEqual(pack._maxCount = PackMaxItems);
+            Assert.AreEqual(pack.GetMaxCount(), PackMaxItems);
+        }
+        [TestMethod]
+        public void VolumeOverflowTest()
+        {
+            const int PackMaxItems = 1000;
+            const float PackMaxVolume = 1;
+            const float PackMaxWeight = 3000;
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.AreEqual(pack.Add(new Water()), false);
+        }
+        [TestMethod]
+        public void WeightOverflowTest()
+        {
+            const int PackMaxItems = 1000;
+            const float PackMaxVolume = 5000;
+            const float PackMaxWeight = 1;
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.AreEqual(pack.Add(new Sword()), false);
+        }
+        [TestMethod]
+        public void AddSingleItemToEmptyPackTest()
+        {
+            const int PackMaxItems = 10;
+            const float PackMaxVolume = 20;
+            const float PackMaxWeight = 30;
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.AreEqual(pack.Add(new Arrow()), true);
+        }
+        [TestMethod]
+        public void AddItemsExceedingConstraintsTest()
+        {
+            const int PackMaxItems = 2;
+            const float PackMaxVolume = 20;
+            const float PackMaxWeight = 30;
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.AreEqual(pack.Add(new Water()), true);
+            Assert.AreEqual(pack.Add(new Food()), true);
+            Assert.AreEqual(pack.Add(new Sword()), false);
+        }
+        [TestMethod]
+        public void AddItemsExactConstraintsTest()
+        {
+            const int PackMaxItems = 4;
+            const float PackMaxVolume = 20;
+            const float PackMaxWeight = 30;
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.AreEqual(pack.Add(new Water()), true);
+            Assert.AreEqual(pack.Add(new Food()), true);
+            Assert.AreEqual(pack.Add(new Sword()), true);
+            Assert.AreEqual(pack.Add(new Bow()), true);
+        }
+        [TestMethod]
+        public void PackClassNegativeValuesTest()
+        {
+            const int PackMaxItems = -3;
+            const float PackMaxVolume = -5;
+            const float PackMaxWeight = -10;
+            Pack pack = new(PackMaxItems, PackMaxVolume, PackMaxWeight);
+            Assert.AreEqual(PackMaxItems, pack.GetMaxCount());
         }
     }
 }
